a5f59b97b4b44b4817252b22bec81444b8e0e235
Jason McIver <mciver.jason@gmail.com>
2025-01-02 14:46:41 +1100

    delete line

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,6 @@ const PORT = 8080;
 const GITHUB_TLDR_PR_WEBHOOK_SECRET =
   process.env.GITHUB_TLDR_PR_WEBHOOK_SECRET || "REPLACE_ME_PLEASE";
 
-// blah
-
 // GitHub App credentials:
 const GITHUB_TLDR_PR_APP_ID = process.env.GITHUB_TLDR_PR_APP_ID || "";
 const GITHUB_TLDR_PR_PRIVATE_KEY = (
